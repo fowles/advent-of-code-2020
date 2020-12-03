6e2ad8f7e51461f6d1f3fc2be029221f8b35eb94
Matt Kulukundis <matt.fowles@gmail.com>
2020-12-03 11:04:38 -0500

    Switch to FromStr trait for day2.

--- src/day2.rs
+++ src/day2.rs
@@ -1,5 +1,3 @@
-use text_io::scan;
-
 #[derive(Default)]
 struct PasswordRecord {
     password: String,
@@ -8,18 +6,22 @@ struct PasswordRecord {
     required: String,
 }
 
+impl std::str::FromStr for PasswordRecord {
+    type Err = Box<text_io::Error>;
+
+    fn from_str(s: &str) -> Result<PasswordRecord, Self::Err> {
+        let mut r: PasswordRecord = Default::default();
+        text_io::try_scan!(
+                s.bytes() => "{}-{} {}: {}",
+                r.min, r.max, r.required, r.password);
+        Ok(r)
+    }
+}
 
 #[aoc_generator(day2)]
 fn gen(input: &str) -> Vec<PasswordRecord> {
-    input.lines()
-        .map(|s| {
-            let mut r: PasswordRecord = Default::default();
-            scan!(s.bytes() => "{}-{} {}: {}", r.min, r.max, r.required, r.password);
-            r
-        })
-        .collect()
+    input.lines().map(|s| s.parse().unwrap()).collect()
 }
-
 
 #[aoc(day2, part1)]
 fn part1(passwords: &[PasswordRecord]) -> usize {
@@ -37,8 +39,8 @@ fn part1(passwords: &[PasswordRecord]) -> usize {
 fn part2(passwords: &[PasswordRecord]) -> usize {
     let mut clean: usize = 0;
     for p in passwords {
-        let p1 = &p.password[p.min - 1 .. p.min];
-        let p2 = &p.password[p.max - 1 .. p.max];
+        let p1 = &p.password[p.min - 1..p.min];
+        let p2 = &p.password[p.max - 1..p.max];
         let r = &p.required;
         if p1 == r && p2 == r {
             continue;
@@ -49,4 +51,3 @@ fn part2(passwords: &[PasswordRecord]) -> usize {
     }
     return clean;
 }
-
